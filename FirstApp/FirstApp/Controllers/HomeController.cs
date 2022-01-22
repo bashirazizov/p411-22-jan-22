7f1dc1d093745a63e820cf4421084808265ff9b2
bashirazizov <bashir.azizov@code.edu.az>
2022-01-22 14:44:16 +0400

    init

---
+++ FirstApp/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //public IActionResult Index(string name, string surname)
+        //{
+        //    ViewData["name"] = name;
+        //    ViewBag.surname = surname;
+        //    return View();
+        //}
+
+        public IActionResult About(bool haveSeenHome)
+        {
+            //ViewBag.name = "Mamed";
+            TempData["name"] = "Mamed";
+            if (haveSeenHome)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
